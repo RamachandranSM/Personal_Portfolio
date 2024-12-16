c528f30b51de0a411f8060c29e60f5e0323e1864
RamachandranSM <ramachandrans212004@gmail.com>
2024-12-16 19:20:29 +0530

    first commit

---
+++ Portfolio_Ram/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';  
+import './App.css';
+import Card from './Card'; 
+function App() {
+  return (
+    <div className="profile-page">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
